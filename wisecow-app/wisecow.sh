4c5ecab1bb144b3991802ecab1e41ae96db44ef6
Ubuntu <ubuntu@ip-172-31-87-80.ec2.internal>
2024-02-01 09:57:41 +0000

    new commit

--- wisecow-app/wisecow.sh
+++ wisecow-app/wisecow.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-# Generate the output of Cowsay with a custom message
-function generate_cowsay() {
+function generate_wisecow() {
     echo "Hello Shrey" | /usr/games/cowsay
 }
 
-# Listen on port 4499 for incoming connections and serve the output of Cowsay
-function serve_cowsay() {
-    # Print a message indicating that the script is serving Cowsay
-    echo "Serving the output of Cowsay on port 4499..."
+function serve_wisecow() {
+    echo "Serving the output of wisecow on port 4499..."
 
-    # Serve the output as HTML
     while true; do
-        # Generate the output of Cowsay with the custom message
-        output=$(generate_cowsay)
+        output=$(generate_wisecow)
         # Serve the output as HTML
         echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title>Cowsay Output</title></head><body><pre>$output</pre></body></html>" | nc -l -p 4499
     done
 }
 
-# Start serving Cowsay
-serve_cowsay
+# Start serving wisecow
+serve_wisecow
 
